11a1370783a069771f6fd9659f5489abfc5c1b42
Yunus Emre <yunuzemre30@gmail.com>
2024-09-27 15:50:58 +0200

    redux-productSlice

--- src/redux/productSlice.jsx
+++ src/redux/productSlice.jsx
@@ -1,33 +1,15 @@
-
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    products: [],
-    keyword: "",
-}
+  products: [],
+  keyword: "",
+};
 
 const productSlice = createSlice({
-    name: "product",
-    initialState,
-    reducers: {
-        sortingProducts: (state, action) => {  //? sıralama yapar artan,azalan
-            state.products = [
-                ...state.products.sort((a, b) =>
-                    action.payload == "asc"
-                        ? a.price - b.price
-                        : action.payload == "desc"
-                            ? b.price - a.price
-                            : null
-                )
-            ]
-        },
-
-        addNewProduct: (state, action) => {
-            state.products = [...state.products, action.payload]
-        }
-    }
-})
-
+  name: "product",
+  initialState,
+  reducers: {
+    
 //!   Parametreler: state ve action
 //!   state: Mevcut durumu temsil eder.
 //!   action: İşlemi temsil eder ve genellikle bir type ve payload içerir.
@@ -35,3 +17,47 @@ const productSlice = createSlice({
 //!   action.payload değeri "asc" ise, ürünleri fiyatlarına göre artan sırada sıralar.
 //!   action.payload değeri "desc" ise, ürünleri fiyatlarına göre azalan sırada sıralar.
 //!   action.payload değeri ne "asc" ne de "desc" ise, sıralama yapılmaz (bu durumda null döner).
+    sortingProducts: (state, action) => {
+      state.products = [
+        ...state.products.sort((a, b) =>
+          action.payload == "asc"
+            ? a.price - b.price
+            : action.payload == "desc"
+              ? b.price - a.price
+              : null
+        ),
+      ];
+    },
+    addNewProduct: (state, action) => {
+      state.products = [...state.products, action.payload];
+    },
+
+    deleteProduct: (state, action) => {
+      state.products = [
+        ...state.products.filter((product) => product.id != action.payload),
+      ];
+    },
+    updateProduct: (state, action) => {
+      state.products = [
+        ...state.products.map((product) =>
+          product.id == action.payload.id
+            ? { ...product, ...action.payload }
+            : product
+        ),
+      ];
+    },
+    filterProducts: (state, action) => {
+      state.keyword = action.payload;
+    },
+  },
+});
+
+export const {
+  addNewProduct,
+  deleteProduct,
+  updateProduct,
+  sortingProducts,
+  filterProducts,
+} = productSlice.actions;
+export default productSlice.reducer;
+
