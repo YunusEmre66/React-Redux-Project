40596a8f22c6282b12c1cee7d3107360bf6affb6
Yunus Emre <yunuzemre30@gmail.com>
2024-09-27 15:42:51 +0200

    productSlice

--- src/redux/productSlice.jsx
+++ src/redux/productSlice.jsx
@@ -2,5 +2,36 @@
 
 
 const initialState = {
-    products: []
+    products: [],
+    keyword: "",
 }
+
+const productSlice = createSlice({
+    name: "product",
+    initialState,
+    reducers: {
+        sortingProducts: (state, action) => {  //? sıralama yapar artan,azalan
+            state.products = [
+                ...state.products.sort((a, b) =>
+                    action.payload == "asc"
+                        ? a.price - b.price
+                        : action.payload == "desc"
+                            ? b.price - a.price
+                            : null
+                )
+            ]
+        },
+
+        addNewProduct: (state, action) => {
+            state.products = [...state.products, action.payload]
+        }
+    }
+})
+
+//!   Parametreler: state ve action
+//!   state: Mevcut durumu temsil eder.
+//!   action: İşlemi temsil eder ve genellikle bir type ve payload içerir.
+//!   İşlem: Ürünleri sıralar.
+//!   action.payload değeri "asc" ise, ürünleri fiyatlarına göre artan sırada sıralar.
+//!   action.payload değeri "desc" ise, ürünleri fiyatlarına göre azalan sırada sıralar.
+//!   action.payload değeri ne "asc" ne de "desc" ise, sıralama yapılmaz (bu durumda null döner).
